2c1e5ca742256c56ad6fa7cd90ce3e268279c6e1
Mason <musong.choi@cj.net>
2022-09-07 06:19:31 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello Mason Test Code!</h1>
+      <h1>Hello Mason!</h1>
       <h2>Start editing to RT see some magic happen!</h2>
     </div>
   );
